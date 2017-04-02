d6c64b445dd2429baf890de8e139a34625f01aac
Benedikt Müller <mueller@me.com>
2017-04-02 08:57:25 +0200

    Implement configuration node and message post node

---
+++ wws-application/wws-application.js
@@ -0,0 +1,54 @@
+var request = require("request");
+var rp = require("request-promise-native");
+
+module.exports = function(RED) {
+  function wwsApplicationNode(config) {
+    RED.nodes.createNode(this, config);
+    this.appID = config.appID;
+    this.appSecret = config.appSecret;
+    this.auth = {
+      accessToken: null,
+      tokenExpiry: null,
+      jti: null
+    }
+
+    this.getAccessToken = () => {
+      var currentMilliseconds = new Date().valueOf();
+      if(this.auth.accessToken && (this.auth.tokenExpiry > currentMilliseconds)) {
+        console.log("Token still valid.");
+        return Promise.resolve(this.auth);
+      } else {
+        console.log("New token required.");
+        return getToken(this.appID, this.appSecret).then((body) => {
+          this.auth.accessToken = body.access_token;
+          this.auth.jti = body.jti;
+          this.auth.tokenExpiry = currentMilliseconds + (body.expires_in * 1000);
+          console.log("Successfully retrieved access token.");
+          return this.auth;
+        }).catch(function(err) {
+          console.log("Error while fetching access token." + err)
+        });
+      }
+    }
+  }
+  RED.nodes.registerType("wws-application", wwsApplicationNode);
+
+  // Helper functions
+  function getToken(appID, appSecret) {
+    var authorization = new Buffer(appID + ":" + appSecret).toString('base64');
+    var host = "https://api.watsonwork.ibm.com";
+    var uri = host + "/oauth/token";
+    var options = {
+      method: "POST",
+      uri: uri,
+      headers: {
+        Authorization: "Basic " + authorization
+      },
+      form: {
+        grant_type: "client_credentials"
+      },
+      json: true
+    }
+    return rp(options);
+  }
+}
---
+++ wws-message-post/wws-message-post.js
@@ -0,0 +1,62 @@
+var request = require("request");
+var rp = require("request-promise-native");
+
+module.exports = function(RED) {
+  function wwsMessagePostNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    this.on("input", function(msg) {
+      this.application = RED.nodes.getNode(config.application);
+      if(this.application) {
+        this.application.getAccessToken().then(function(auth) {
+          var actor = {
+            avatar: "",
+            name: "Node Red",
+            url: ""
+          }
+          wwsMessagePost(config.space, auth.accessToken, actor, "red", msg.payload, msg.topic).then(() => {
+            console.log("Successfully posted message to WWS.");
+          }).catch((err) => {
+            console.log("Error while posting message to WWS.", err);
+          });
+        }).catch(function(err) {
+          console.log("Error while asking for access token.", err);
+        });
+      } else {
+        this.error("No WWS Application configured.");
+      }
+    });
+  }
+  RED.nodes.registerType("wws-message-post", wwsMessagePostNode);
+
+  // Helper functions
+  function wwsMessagePost(space, accessToken, actor, color, text, title) {
+    var host = "https://api.watsonwork.ibm.com";
+    var uri = host + "/v1/spaces/" + space + "/messages";
+    var options = {
+      method: "POST",
+      uri: uri,
+      headers: {
+        Authorization: "Bearer " + accessToken
+      },
+      json: true,
+      body: {
+        annotations: [{
+          actor: {
+            avatar: actor.avatar,
+            name: actor.name,
+            url: actor.url
+          },
+          color: color,
+          text: text,
+          title: title,
+          type: "generic",
+          version: 1
+        }],
+        type: "appMessage",
+        version: 1
+      }
+    };
+    return rp(options);
+  }
+}
