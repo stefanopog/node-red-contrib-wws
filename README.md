59d98e03df09a1a5593b509257d2d61ae19aa030
Stefano Pogliani <Stefano.Pogliani@fr.ibm.com>
2018-06-05 10:41:00 +0200

    validate Action node

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ node-red-contrib-wws
 A set of node-red nodes to interact with [IBM Watson Workspace](https://workspace.ibm.com/).
 
 # Purpose
-This node is intended to be used for communicating with Watson Workspace APIs using [Workspace Apps](https://developer.watsonwork.ibm.com/docs/get-started/what-can-you-build). Once you have created a Workspace App, you can configure the corresponding account within node-red representing this app.
-Now you have the ability to configure webhooks and send messages or files to spacesto which the App belongs to.
+This node is intended to be used for communicating with Watson Workspace APIs using [Workspace Apps](https://developer.watsonwork.ibm.com/docs/get-started/what-can-you-build). Once you have created a Workspace App, you can create the corresponding "NodeRed Configuration node" representing this app.
+With these nodes, you have the ability to configure webhooks, to send messages or files to spaces to which the corresponding Workspace App is attached, to execute [graphQL commands](https://developer.watsonwork.ibm.com/docs/developers-guide/graphql-api) and to validate "ActionSelected" annotations.
+
+Here is the list of Nodes currently proposed by this package.
 
 ![Watson Work Palette](help/palette_view.png)
 
@@ -17,6 +19,9 @@ The following packages are required to be present in node-red prior to this inst
 * `"simple-oauth2": "^1.5.2"`
 * `"javascript-state-machine":"^3.0.1"`
 * `"follow-redirects":"^1.3.0"`
+* `"request-promise-native": "^1.0.5"`
+* `"request": "^2.87.0"`
+
 
 ## Manual Installation
 * In case of manual installation copy the code to the user directory of Node-RED e.g. `/data/node_modules/node-red-contrib-wws`.
@@ -28,6 +33,7 @@ The following packages are required to be present in node-red prior to this inst
   * creates a configuration node for a dedicated Watson Work Application, 
   * containing App ID and App Secret, 
   * plus additional OAuth configuration elements
+  * Note : **this node is used by all the other nodes**
 * webhook node
   * Configure the webhook url
   * converts events into node-red friendly messages
@@ -37,18 +43,22 @@ The following packages are required to be present in node-red prior to this inst
   * provides a configuration interface (including preview functionality)
   * selection of available spaces
   * availability to upload a profile photo for each app
-  * automatically authenticates the app using the 'Authenticate as App' flow
   * returns response from WWS to msg.payload
 * file node
-  * automatically authenticates the app using the 'Authenticate as App' flow
   * selection of available spaces
   * optional usage of a different access token (e.g. from 'Authenticate on behalf of a user')
   * either sends a file (of any kind) or an image (png, jpeg or gif) to a space
 * graphQL node
   * static or dynamic GraphQL query 
-  * automatically authenticates the app using the 'Authenticate as App' flow
   * optional usage of a different access token (e.g. from 'Authenticate on behalf of a user')
   * return node-red friendly results.
+  * supports **BETA** and **EXPERIMENTAL** APIs
+* Validate Action node
+  * this is a service node which makes it easy to filter Annotations coresponding to **actionSelected** lenses
+  * you can specify multiple **ActionId** (they can come from *custom intents*, from *slash commands*, from *buttons* etc)
+  * provides multiple outputs. Each output corresponds to one of the possible actionId that the instance of the node supports
+  * see ![Validate Action](wws-graphql/icons/wws-action-desc.png)
+
    
 # Known Issues
 * There is an issue with the additional input parameter `operationName` or `variables`. The best workaround is to ignore those additional parameters and stick to the pure graphQL query parameter.
--- wws-graphql/wws-graphql.js
+++ wws-graphql/wws-graphql.js
@@ -1,32 +1,32 @@
 var request = require("request");
 var rp = require("request-promise-native");
 
-module.exports = function(RED) {
+module.exports = function (RED) {
+  //
+  //  Generic graphQL Node
+  //
   function wwsGraphQLNode(config) {
     RED.nodes.createNode(this, config);
     this.application = RED.nodes.getNode(config.application);
-    this.viewType = config.viewType;
     var node = this;
 
     //Check for token on start up
     const tokenFsm = node.application.getStateMachine();
     if (!tokenFsm) {
-        node.error("Please configure your account information first!");
+      node.error("Please configure your account information first!");
     }
-
-    this.isInitialized = () => {
-        var initialized = false;
-        if (tokenFsm.getAccessToken()){
-            node.status({fill: "green", shape: "dot", text: "token available"});
-            initialized = true;
-        } else {
-            node.status({fill: "grey", shape: "dot", text: "uninitialized token"});
-        }
-        return initialized;
+    if (!this.isInitialized()) {
+      const intervalObj = setInterval(() => {
+        if (this.isInitialized()) {
+          this.releaseInterval(intervalObj);
+        };
+      }, 2000);
     };
 
     this.on("input", (msg) => {
       if (!msg.payload) {
+        console.log("No Account Info");
+        node.status({fill:"red", shape:"dot", text:"No Account Info"});
         node.error("Missing required input in msg object: payload");
         return;
       }
@@ -36,51 +36,248 @@ module.exports = function(RED) {
       var bearerToken = msg.token || accessToken.token.access_token;
       var host = this.application.api;
 
-      wwsGraphQL(bearerToken, host, msg._msgid, msg.payload, msg.operationName, msg.variables, this.viewType).then((res) => {
-        this.status({ fill: "green", shape: "dot", text: "Sending query..." });
+      var viewType = "PUBLIC";
+      if (config.wwsBetaFeatures) viewType += ',BETA';
+      if (config.wwsExperimentalFeatures) viewType += ',EXPERIMENTAL';
+
+      console.log('viewType = ' + viewType);
+
+      wwsGraphQL(bearerToken, host, msg.payload, msg.operationName, msg.variables, viewType).then((res) => {
         if (res.errors) {
           msg.payload = res.errors;
+          node.status({fill: "red", shape: "dot", text: "Errors from query"});
+          console.log('errors from query');
+          console.log(JSON.stringify(res.errors));
         } else {
           msg.payload = res.data;
+          node.status({fill: "green", shape: "dot", text: "graphQL Query success"});
+          console.log('Success from graphQL query');
+          console.log(JSON.stringify(res.data));
         }
         node.send(msg);
       }).catch((err) => {
         console.log("Error while posting GraphQL query to WWS.", err);
-        this.status({ fill: "red", shape: "ring", text: "Sending query failed..." });
+        node.status({fill: "red", shape: "ring", text: "Sending query failed..."});
       });
-      setTimeout(() => {
-          node.isInitialized();
-      }, 2000);
+      setTimeout(() => {node.isInitialized(); }, 2000);
     });
-    this.releaseInterval = (intervalObj) => {
-      clearInterval(intervalObj);
-    };
+  }
+
+
+  //
+  //  This node gets the Annotation referred to by a message containing the "Action-Selected" actionId
+  //
+  function wwsValidateActions(config) {
+    RED.nodes.createNode(this, config);
+    this.application = RED.nodes.getNode(config.application);
+    var node = this;
+
+    //Check for token on start up
+    const tokenFsm = node.application.getStateMachine();
+    if (!tokenFsm) {
+      console.log("No Account Info");
+      node.status({fill:"red", shape:"dot", text:"No Account Info"});
+      node.error("Please configure your account information first!");
+    }
     if (!this.isInitialized()) {
-        const intervalObj = setInterval(() => {
-            if (this.isInitialized()) {
-                this.releaseInterval(intervalObj);
-            };
-          }, 2000);
+      const intervalObj = setInterval(() => {
+        if (this.isInitialized()) {
+          this.releaseInterval(intervalObj);
+        };
+      }, 2000);
     };
+
+    this.on("input", (msg) => {
+      var referralMessageId;
+      var actionId;
+      var actionList;
+
+      if ((config.wwsReferralMessageId === '') && 
+          ((msg.wwsReferralMessageId === undefined) || (msg.wwsReferralMessageId === ''))) {
+        //
+        //  There is an issue
+        //
+        console.log("Missing referralMessageId Information");
+        node.status({fill:"red", shape:"dot", text:"Missing referralMessageId"});
+        node.error('Missing Bookmark URL', msg);
+        return;
+      }
+      if (config.wwsReferralMessageId !== '') {
+        referralMessageId = config.wwsReferralMessageId.trim();
+      } else {
+        referralMessageId = msg.wwsReferralMessageId.trim();
+      }
+
+      if ((config.wwsActionId === '') && 
+          ((msg.wwsActionId === undefined) || (msg.wwsActionId === ''))) {
+        //
+        //  There is an issue
+        //
+        console.log("Missing actionId Information");
+        node.status({fill:"red", shape:"dot", text:"Missing actionId"});
+        node.error('Missing actionsId', msg);
+        return;
+      }
+      if (config.wwsActionId !== '') {
+        actionId = config.wwsActionId.trim();
+      } else {
+        actionId = msg.wwsActionId.trim();
+      }
+
+      if ((config.wwsActionsList === '') && 
+          ((msg.wwsActionsList === undefined) || (msg.wwsActionsList === ''))) {
+        //
+        //  There is an issue
+        //
+        console.log("Missing ActionsList Information");
+        node.status({fill:"red", shape:"dot", text:"Missing ActionsList"});
+        node.error('Missing ActionsList', msg);
+        return;
+      }
+      if (config.wwsActionsList !== '') {
+        actionList = config.wwsActionsList.split(',');
+      } else {
+        actionList = msg.wwsActionsList.split(',');
+      }
+      //
+      //  Check that the action to referred to by the incoming actions-selected is in the list
+      //
+      var selectedRule = -1;
+      for (let i=0; i < actionList.length; i++) {
+        if (matchRuleShort(actionId, actionList[i].trim())) {
+            selectedRule = i;
+          break;
+        }
+      }
+      if (selectedRule === -1) {
+        //
+        //  Build an output array of messages where all the messages are NULL except the Last one
+        //
+        console.log('ActionId ' + actionId + ' does not match input ActionsList');
+        var outArray = [];
+        for (let i=0; i < actionList.length; i++) {
+          outArray.push(null);
+        }
+        outArray.push({payload : actionId});
+        node.send(outArray);
+        return;
+      }
+
+      //
+      //  At this point, we know that we are trying to match an ActionId which is in the list
+      //
+      //  If the ActionId represents a SLASH Command, we do not have to do much....
+      //
+      if (actionId.startsWith('/')) {
+          //
+          //  Build the output Array (as the node has multiple outputs)
+          //  all the outputs will be initialized to NULL
+          //
+          var outArray = [];
+          for (let i=0; i <= actionList.length; i++) {
+            outArray.push(null);
+          }
+          //
+          //  the array item corresponding to the selectedRule is filled with the result
+          //
+          outArray[selectedRule] = msg;
+          //  
+          //  Sends the output array
+          //
+          node.send(outArray);        
+      }
+      //
+      //  If the ActionId is not a SLASH Command, then we need to get the one annotation from the referralMsessageId which
+      //  corresponds to the Actios ID.
+      //  So we are ready to build the graphQL query to retrieve the annotations 
+      //
+      var accessToken = this.application.verifyAccessToken(tokenFsm.getAccessToken(), this);
+      var bearerToken = msg.token || accessToken.token.access_token;
+      var host = this.application.api;
+      var query = 'query getAnnotations { message(id: "' + referralMessageId + '"){annotations}}';
+
+      //
+      //  Retrieve the annotations for the given Message
+      //
+      wwsGraphQL(bearerToken, host, query, '', '', "PUBLIC")
+      .then((res) => {
+        if (res.errors) {
+          //
+          //  Should NOT BE...
+          //
+          msg.payload = res.errors;
+          node.status({fill: "red", shape: "dot", text: "Errors from query"});
+          console.log('errors from query');
+          console.log(JSON.stringify(res.errors));
+          node.error('Missing ActionsList', msg);
+        } else {
+          //
+          //  Ok, we got the array of annotations...
+          //
+          node.status({fill: "green", shape: "dot", text: "Annotations retrieved..."});
+          console.log('Success from graphQL query : Annotations retrieved');
+          console.log(JSON.stringify(res.data));
+          //
+          //  Now we have the annotations. Check to find the one that is "message-focus" and corresponds to the lens=ActionId
+          //
+          if (res.data.message !== null) {
+            for (let i=0; i < res.data.message.annotations.length; i++) {
+              let intent = JSON.parse(res.data.message.annotations[i]);
+              if ((intent.type === "message-focus") && (intent.lens === actionId)) {
+                  msg.payload = intent;
+                break;
+              }
+            }
+          } else {
+            msg.payload = {};
+          }
+          //
+          //  Build the output Array (as the node has multiple outputs)
+          //  all the outputs will be initialized to NULL
+          //
+          var outArray = [];
+          for (let i=0; i <= actionList.length; i++) {
+            outArray.push(null);
+          }
+          //
+          //  the array item corresponding to the selectedRule is filled with the result
+          //
+          outArray[selectedRule] = msg;
+          //  
+          //  Sends the output array
+          //
+          node.send(outArray);
+        }})
+      .catch((err) => {
+        msg.payload = err;
+        console.log("Error while posting GraphQL query to WWS.", err);
+        node.status({fill: "red", shape: "ring", text: "Sending query failed..."});
+        node.error('Error while posting GraphQL query to WWS.', msg);
+      });
+      setTimeout(() => {node.isInitialized();}, 2000);
+    });
   }
 
   RED.nodes.registerType("wws-graphql", wwsGraphQLNode);
 
-  // Helper functions
-  function wwsGraphQL(accessToken, host, requestId, query, operationName, variables, viewType) {
+  RED.nodes.registerType("wws-validateActions", wwsValidateActions);
+
+  //
+  //  Helper functions
+  //
+  function wwsGraphQL(accessToken, host, query, operationName, variables, viewType) {
     var uri = host + "/graphql";
     if (operationName) {
-      uri +="?operationName="+operationName; 
+      uri += "?operationName=" + operationName;
     }
     if (variables) {
-      uri += (uri.includes("?") ? "&" : "?") + "variables="+variables;
+      uri += (uri.includes("?") ? "&" : "?") + "variables=" + variables;
     }
     var options = {
       method: "POST",
       uri: uri,
       headers: {
         "Authorization": "Bearer " + accessToken,
-        "X-RequestId": requestId,
         "x-graphql-view": viewType
       },
       json: true,
@@ -90,4 +287,59 @@ module.exports = function(RED) {
     };
     return rp(options);
   }
+
+  //
+  //  This code comes form the following article : https://stackoverflow.com/questions/26246601/wildcard-string-comparison-in-javascript
+  //
+  //
+  //  This is the "Short code"
+  //
+  function matchRuleShort(str, rule) {
+    return new RegExp("^" + rule.split("*").join(".*") + "$").test(str);
+  }
+  //
+  //  And this is the full code which serves as explanation to the "short code" 
+  //
+  function matchRuleExpl(str, rule) {
+    // "."  => Find a single character, except newline or line terminator
+    // ".*" => Matches any string that contains zero or more characters
+    rule = rule.split("*").join(".*");
+
+    // "^"  => Matches any string with the following at the beginning of it
+    // "$"  => Matches any string with that in front at the end of it
+    rule = "^" + rule + "$"
+
+    //Create a regular expression object for matching string
+    var regex = new RegExp(rule);
+
+    //Returns true if it finds a match, otherwise it returns false
+    return regex.test(str);
+  }
+  //
+  //  Examples
+  //
+  //alert(
+  //  "1. " + matchRuleShort("bird123", "bird*") + "\n" +
+  //  "2. " + matchRuleShort("123bird", "*bird") + "\n" +
+  //  "3. " + matchRuleShort("123bird123", "*bird*") + "\n" +
+  //  "4. " + matchRuleShort("bird123bird", "bird*bird") + "\n" +
+  //  "5. " + matchRuleShort("123bird123bird123", "*bird*bird*") + "\n"
+  //);
+  //
+  //  End of code coming form the following article : https://stackoverflow.com/questions/26246601/wildcard-string-comparison-in-javascript
+  //  
+
+  this.isInitialized = () => {
+    var initialized = false;
+    if (tokenFsm.getAccessToken()) {
+      node.status({fill: "green", shape: "dot", text: "token available"});
+      initialized = true;
+    } else {
+      node.status({fill: "grey", shape: "dot", text: "uninitialized token"});
+    }
+    return initialized;
+  };
+  this.releaseInterval = (intervalObj) => {
+    clearInterval(intervalObj);
+  };
 }
