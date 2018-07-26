9ae460ccde4ecbef289b917a64dd45bb8bf5ef32
Arne Sutor <arne.sutor@de.ibm.com>
2018-07-26 22:22:02 +0200

    New Node wws-token-viewer
    A dedicated token to check the status of the outh token.
    Based on a refresh period configured [0 - 60] the token is getting
    refreshed automatically.
    
    This is useful for call which are not executed within the grace period
    of an expired user token (max 2h).
    
    By this it will be assured that the token will be refreshed
    automatically.

---
+++ wws-token-viewer/wws-token-viewer.js
@@ -0,0 +1,71 @@
+module.exports = function(RED) {
+    "use strict";
+    const OAuth2 = require('simple-oauth2');
+    function wwsTokenViewer(config) {
+        RED.nodes.createNode(this, config);
+        this.application = RED.nodes.getNode(config.application);
+        var node = this;
+
+        //Check for token on start up
+        if (!node.application) {
+            node.error("Please configure your Watson Workspace App first!");
+            node.status({fill: "red", shape: "dot", text: "token unavailable"});
+            return;
+        }
+
+        var intervalObj = undefined;
+        console.log("refreshPeriod: " + config.refreshPeriod);
+        if (config.refreshPeriod>0) {
+            let pollingPeriod = config.refreshPeriod*60*1000;
+
+            intervalObj = setInterval(() => {
+                node.status({fill: "yellow", shape: "dot", text: "refreshing token..."});
+                node.application.refreshToken()
+                .then((success) => {
+                    node.warn("Token " + success.id + " has been refreshed!")
+                    node.status({fill: "green", shape: "dot", text: "token refreshed"});
+                })
+                .catch((error) => {
+                    node.error(error);
+                    node.status({fill: "red", shape: "dot", text: "token uninitialized"});
+                });
+            }, pollingPeriod);
+        }
+        node.checkToken(config.application);
+
+        this.on("input", function(msg) {
+            //Input validation
+            node.checkToken(config.application);
+
+            //just bypassing the msg
+            node.send(msg);
+
+        });
+        this.on('close', function(removed, done) {
+            clearInterval(intervalObj);
+            if (removed) {
+                // This node has been deleted
+            } else {
+                // This node is being restarted
+            }
+            done();
+        });
+    }
+    RED.nodes.registerType("wws-token-viewer", wwsTokenViewer);
+
+    wwsTokenViewer.prototype.checkToken = function(id) {
+        var node = this;
+        let credentials = RED.nodes.getCredentials(id);
+        const oauth2 = OAuth2.create(node.application.getCredentials());
+        if (credentials.token) {
+            let tokenHelper = oauth2.accessToken.create(credentials.token);
+            if (tokenHelper.expired()) {
+                node.status({fill: "yellow", shape: "dot", text: "token expired"});
+            } else {
+                node.status({fill: "green", shape: "dot", text: "token available"});
+            }
+        } else {
+            node.status({fill: "red", shape: "dot", text: "token unavailable"});
+        }
+    }
+};
